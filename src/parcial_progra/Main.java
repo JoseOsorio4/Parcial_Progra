d9c81a5fdcbbc737329fc72bbdacd816d930d365
JoseOsorio4 <josorioo4@miumg.edu.gt>
2026-03-04 23:23:14 -0600

    comprobacion de commits

--- src/parcial_progra/Main.java
+++ src/parcial_progra/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundooooo");
 		System.out.println("Comprobacion de commits y cargas al sistema");
+		System.out.println("Prueba de commit");
 
 	}
 
