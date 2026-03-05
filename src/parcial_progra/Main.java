3dbe1f1b796c1e10c48932cdfb3e8918f235ddfb
JoseOsorio4 <josorioo4@miumg.edu.gt>
2026-03-04 23:03:15 -0600

    Mi primer commit

---
+++ src/parcial_progra/Main.java
@@ -0,0 +1,11 @@
+package parcial_progra;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundooooo");
+
+	}
+
+}
